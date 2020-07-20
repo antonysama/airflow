109701360ca4266c3305ce96f61d3aea548551fa
Antony Samarawickrema <antonysama@gmail.com>
2020-07-19 21:05:13 -0600

    Added my project

---
+++ dags/dags.py
@@ -0,0 +1,31 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators import EmailOperator
+from datetime import datetime, timedelta
+default_args = {
+        'owner': 'airflow',
+        'start_date': datetime(2020, 7, 15),
+        'schedule_interval': '@daily',
+        }
+
+dag = DAG('edmjnl_crawler8', default_args=default_args, 
+schedule_interval='@hourly', catchup=True)
+
+t1 = BashOperator(
+    task_id='schedule_edmjnl_crawler',
+    bash_command="cd ~/airflow/edmjnl && scrapy crawl edmjnl_crawl -o file.csv -t csv",
+    dag=dag) 
+
+#For ^ I prefer: file_'{{ execution_date }}'.csv -t csv"
+    
+t2 = EmailOperator(
+    task_id='schedule_edmjnl_email',
+    to='antonysama@gmail.com',
+    subject='Edmjnl Email',
+    html_content="HTML content",
+    files='file.csv',
+    dag=dag) 
+
+# For ^ I need something like ["home/antony/airflow/edmjnl/file_'{{ execution_date }}'.csv"] 
+    
+t1>>t2    
