c034107c5e2834b6ed0f73dfe9c330106dfd5387
Antony Samarawickrema <antonysama@gmail.com>
2020-07-21 20:34:08 -0600

    modified dags.py

--- dags/dags.py
+++ dags/dags.py
@@ -4,21 +4,26 @@ from airflow.operators import EmailOperator
 from datetime import datetime, timedelta
 default_args = {
         'owner': 'airflow',
-        'start_date': datetime(2020, 7, 15),
+        'start_date': datetime(2020, 7, 19),
         'schedule_interval': '@daily',
         }
 
-dag = DAG('edmjnl_crawler8', default_args=default_args, 
+dag = DAG('edmjnl_crawler11', default_args=default_args, 
 schedule_interval='@hourly', catchup=True)
 
 t1 = BashOperator(
     task_id='schedule_edmjnl_crawler',
-    bash_command="cd ~/airflow/edmjnl && scrapy crawl edmjnl_crawl -o file.csv -t csv",
+    bash_command="cd ~/airflow/edmjnl2 && scrapy runspider edmjnl.py -o file.csv -t csv",
     dag=dag) 
 
 #For ^ I prefer: file_'{{ execution_date }}'.csv -t csv"
+
+t2 = BashOperator(
+    task_id='schedule_edmjnl_crawler2',
+    bash_command="cd ~/airflow/edmjnl2 && scrapy runspider post.py -o file2.csv -t csv",
+    dag=dag) 
     
-t2 = EmailOperator(
+t3 = EmailOperator(
     task_id='schedule_edmjnl_email',
     to='antonysama@gmail.com',
     subject='Edmjnl Email',
